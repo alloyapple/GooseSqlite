ac2f11308305c9920226fb370d23d7e4289096a5
color <alloy.apple@gmail.com>
2019-04-02 12:15:39 +0800

    delete unused code

--- Tests/GooseSqliteTests/GooseSqliteTests.swift
+++ Tests/GooseSqliteTests/GooseSqliteTests.swift
@@ -6,7 +6,6 @@ final class GooseSqliteTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(GooseSqlite().text, "Hello, World!")
 
         let database = Database(path: "./sql/password.db")
 
