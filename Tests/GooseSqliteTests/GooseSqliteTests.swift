05f38902acfab022835b808850d015fb6cde78f6
color <alloy.apple@gmail.com>
2019-04-02 15:45:51 +0800

    add commit func in test

--- Tests/GooseSqliteTests/GooseSqliteTests.swift
+++ Tests/GooseSqliteTests/GooseSqliteTests.swift
@@ -1,15 +1,36 @@
 import XCTest
 @testable import GooseSqlite
+import Glibc
+
 
 final class GooseSqliteTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let database = Database(path: ":memory:")
-        XCTAssertTrue(database.open())
-        XCTAssertTrue(database.executeUpdate(sql: "create table test(id integer primary key, name text)", args: []))
-        XCTAssertTrue(database.executeUpdate(sql: "insert into test(name) values (?)", args: ["foo"]))
+
+        var tv = timeval()
+        gettimeofday(&tv, nil)
+
+        let s = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000
+
+
+        let database = Database(path: "./test.db")
+        database.open()
+        database.executeUpdate(sql: "create table test(id integer primary key, name text)", args: [])
+        database.executeUpdate(sql: "insert into test(name) values (?)", args: ["foo"])
+
+        database.beginTransaction()
+        for _ in 1...100000 {
+            database.executeUpdate(sql: "insert into test(name) values (?)", args: ["foo"])
+        }
+        database.commit()
+
+        tv = timeval()
+        gettimeofday(&tv, nil)
+
+        let e = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000
+        print("time \(Double(e - s) / 1000)")
 
 
     }
