17228944fe93862eff1a18347678c7c878a4617d
color <alloy.apple@gmail.com>
2019-04-02 12:48:51 +0800

    add test

--- Tests/GooseSqliteTests/GooseSqliteTests.swift
+++ Tests/GooseSqliteTests/GooseSqliteTests.swift
@@ -6,24 +6,10 @@ final class GooseSqliteTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-
-        let database = Database(path: "./sql/password.db")
-
-        struct TestData: Codable {
-            var password: String
-            var _ID: Int32
-        }
-
-        if database.open() {
-            let rs = database.executeQuery(sql: "SELECT * FROM password", args: [])
-            let data: TestData?  = rs.result()
-
-            if let data = data {
-                print("data base open data \(data)")
-            }
-
-
-        }
+        let database = Database(path: ":memory:")
+        XCTAssertTrue(database.open())
+        XCTAssertTrue(database.executeUpdate(sql: "create table test(id integer primary key, name text)", args: []))
+        XCTAssertTrue(database.executeUpdate(sql: "insert into test(name) values (?)", args: ["foo"]))
 
 
     }
